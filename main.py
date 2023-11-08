d1ce1037314b96e96adce18b2ca80db4aafafd6e
Harshpreet Kaur <114172812+harshpreet14@users.noreply.github.com>
2023-11-09 00:16:20 +0530

    Displaying user_info

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 import pickle
+import numpy as np
 import cv2
-import face_recognition_models
 import face_recognition
 import cvzone
-import numpy as np
 from datetime import datetime
 import csv
 
@@ -23,22 +22,18 @@ for path in modePathList:
 # print(len(imgModeList))
 
 # Load the encoding file
-file = open("EncodeFile.p", "rb")
+print("Loading Encode File ...")
+file = open('EncodeFile.p', 'rb')
 encodeListKnownWithIds = pickle.load(file)
 file.close()
 encodeListKnown, studentIds = encodeListKnownWithIds
 # print(studentIds)
-
-# Load data from csv
-students = []
-with open('students.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        students.append(row)
+print("Encode File Loaded")
 
 modeType = 0
 counter = 0
 id = -1
+imgStudent = []
 
 while True:
     success, img = cap.read()
@@ -50,24 +45,108 @@ while True:
     encodeCurFrame = face_recognition.face_encodings(imgS, faceCurFrame)
 
     imgBackground[162:162 + 480, 55:55 + 640] = img
-    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[0]
+    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
 
-    for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        # print("matches", matches)
-        # print("faceDis", faceDis)
+    if faceCurFrame:
+        for encodeFace, faceLoc in zip(encodeCurFrame, faceCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            # print("matches", matches)
+            # print("faceDis", faceDis)
 
-        matchIndex = np.argmin(faceDis)
-        # print("Match Index", matchIndex)
-        if matches[matchIndex]:
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
-            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
-            id = studentIds[matchIndex]
+            matchIndex = np.argmin(faceDis)
+            # print("Match Index", matchIndex)
 
-    #   cv2.imshow("Face Attendance", img)
+            if matches[matchIndex]:
+                # print("Known Face Detected")
+                # print(studentIds[matchIndex])
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+                imgBackground = cvzone.cornerRect(imgBackground, bbox, rt=0)
+                student_id = studentIds[matchIndex]
+                if counter == 0:
+                    cvzone.putTextRect(imgBackground, "Loading", (275, 400))
+                    cv2.imshow("Face Attendance", imgBackground)
+                    cv2.waitKey(1)
+                    counter = 1
+                    modeType = 1
+
+        if counter != 0:
+
+            if counter == 1:
+                # Get student details from CSV
+                with open("students.csv", 'r') as csvfile:
+                    reader = csv.DictReader(csvfile)
+                    for row in reader:
+                        if row['id'] == student_id:
+                            studentInfo = row
+                            break
+                print(studentInfo)
+
+                # Get the Image from the Images folder
+                image_path = os.path.join("Images", f'{id}.png')
+                if os.path.exists(image_path):
+                    array = cv2.imread(image_path)
+                    imgStudent = cv2.cvtColor(array, cv2.COLOR_BGR2RGB)
+
+                    # Update data of attendance
+                    datetimeObject = datetime.strptime(studentInfo['last_attendance_time'], "%Y-%m-%d %H:%M:%S")
+                    secondsElapsed = (datetime.now() - datetimeObject).total_seconds()
+                    print(secondsElapsed)
+
+                    if secondsElapsed > 30:
+                        # Update attendance data
+                        studentInfo['total_attendance'] = str(int(studentInfo['total_attendance']) + 1)
+                        studentInfo['last_attendance_time'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        # Save updated data back to CSV
+                        with open("students.csv", 'a') as csvfile:
+                            fieldnames = ['id', 'last_attendance_time', 'total_attendance']
+                            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                            writer.writerow(studentInfo)
+                else:
+                    modeType = 3
+                    counter = 0
+                    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+            if modeType != 3:
+
+                if 10 < counter < 20:
+                    modeType = 2
+
+                    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+                    cv2.putText(imgBackground, str(studentInfo['total_attendance']), (861, 125),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(studentInfo['major']), (1006, 550),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(id), (1006, 493),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+                    cv2.putText(imgBackground, str(studentInfo['standing']), (910, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+                    cv2.putText(imgBackground, str(studentInfo['year']), (1025, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+                    cv2.putText(imgBackground, str(studentInfo['starting_year']), (1125, 625),
+                                cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+
+                    (w, h), _ = cv2.getTextSize(str(studentInfo['name']), cv2.FONT_HERSHEY_COMPLEX, 1, 1)
+                    offset = (414 - w) // 2
+                    cv2.putText(imgBackground, str(studentInfo['name']), (808 + offset, 445),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50), 1)
+
+                    imgBackground[175:175 + 216, 909:909 + 216] = imgStudent
+
+                counter += 1
+
+                if counter >= 20:
+                    counter = 0
+                    modeType = 0
+                    studentInfo = []
+                    imgStudent = []
+                    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+    else:
+        modeType = 0
+        counter = 0
+    # cv2.imshow("Webcam", img)
     cv2.imshow("Face Attendance", imgBackground)
     cv2.waitKey(1)
-
